29c7bf20d12b60eb2b4cb6b2bb24766a7b56dca8
Dalton <tealfox101@gmail.com>
2024-04-19 12:54:46 -0500

    update loadable resources to HTTPS

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const emailUsername = process.env.EMAIL_USERNAME;
 const emailPassword = process.env.EMAIL_PASSWORD;
 
 const app = express();
-const port = 3000;
+const port = 8080;
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
